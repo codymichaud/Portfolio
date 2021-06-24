aa5329d19ca1da5561056384208aed4c828472f0
Cody Michaud <cody.r.michaud@gmail.com>
2021-06-24 09:59:06 -0700

    Adding PNG files for project page

--- client/src/components/Projects/index.js
+++ client/src/components/Projects/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import './style.css';
 import lyricity from './img/Lyricity.PNG';
-import
+import socks from './img/socksApp.PNG';
+import leesRept from './img/lees-reptile.PNG';
+import latteDaa from './img/latte.Daa.PNG';
